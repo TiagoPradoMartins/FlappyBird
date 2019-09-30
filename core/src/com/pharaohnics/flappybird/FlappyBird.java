f14691019933c0bed27c9f6baa63e3d287ae9712
TiagoPradoMartins <tiagopradomartins@gmail.com>
2019-09-30 12:22:07 -0300

    Terceira Versao

--- core/src/com/pharaohnics/flappybird/FlappyBird.java
+++ core/src/com/pharaohnics/flappybird/FlappyBird.java
@@ -2,8 +2,10 @@ package com.pharaohnics.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -12,43 +14,52 @@ public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture[] passaros;
 	private Texture fundo;
-	private Texture canoTopo;
 	private Texture canoTopoMaior;
-	private Texture canoBaixo;
 	private Texture canoBaixoMaior;
 	private Random numeroRandomico;
+	private BitmapFont fonte;
 
 
 	//Atributos de configuraçao
-	private int larguraDispositivo;
-	private int alturaDispositivo;
-	private float variacaoPassaro = 0;
-	private float velocidadeQueda = 0;
-	private float posicaoVerticalInicial;
-	private float posicaoMovimentoCanoHorizontal;
-	private float espacoEntreCanos;
-	private float deltaTime;
-	private float alturaEntreCanosRandomica;
+	private int 	larguraDispositivo;
+	private int 	alturaDispositivo;
+	private float 	variacaoPassaro = 0;
+	private float 	velocidadeQueda = 0;
+	private float 	posicaoVerticalInicial;
+	private float 	posicaoMovimentoCanoHorizontal;
+	private float 	espacoEntreCanos;
+	private float 	deltaTime;
+	private float 	alturaEntreCanosRandomica;
+	private int		estadoJogo  = 0;
+	private int     pontuacao   = 0;
 
 
 
 	@Override
 	public void create () {
-		numeroRandomico = new Random();
+		//Gera numero randomico
+	    numeroRandomico = new Random();
+
 		//Cria o SpriteBatch com o nome de batch
 		batch = new SpriteBatch();
+
+        fonte = new BitmapFont(); // cria o BitmapFont com o nome de fonte
+        fonte.setColor(Color.WHITE); // seta a cor da fonte como branca
+        fonte.getData().scale(6); // seta o tamanho da fonte como 6
+
 		//Cria e define as imagens para o passaro
 		passaros  = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
 		passaros[2] = new Texture("passaro3.png");
 
+		//Cria a imagem de fundo
 		fundo 	= new Texture("fundo.png");
 
-		canoBaixo = new Texture("cano_baixo.png");
+        //Cria e define as imagens para os canos
 		canoBaixoMaior = new Texture("cano_baixo_maior.png");
 		canoTopoMaior = new Texture("cano_topo_maior.png");
-		canoTopo = new Texture("cano_topo.png");
+
 
 		//Recebe nos atributos a altura e largura da tela
 		larguraDispositivo 	= Gdx.graphics.getWidth();
@@ -61,35 +72,42 @@ public class FlappyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		deltaTime = Gdx.graphics.getDeltaTime();
+		deltaTime = Gdx.graphics.getDeltaTime(); //Resgata o delta time
 		variacaoPassaro += deltaTime *5 ;
-		posicaoMovimentoCanoHorizontal -= deltaTime * 200;
+		if (variacaoPassaro > 2) variacaoPassaro = 0;
 
-		velocidadeQueda++;
+		if(estadoJogo == 0){
+			if(Gdx.input.justTouched())
+				estadoJogo = 1;
+		}else {
 
-		if(variacaoPassaro > 2)	variacaoPassaro = 0;
+			posicaoMovimentoCanoHorizontal -= deltaTime * 200;
 
-		if (Gdx.input.justTouched())
-			velocidadeQueda = -15;
+			velocidadeQueda++;
 
+			if (Gdx.input.justTouched())
+				velocidadeQueda = -15;
+			//Verifica se o passaro passou do ponto inicial vertical ou se a tela foi pressionada
+			if (posicaoVerticalInicial > 0 || velocidadeQueda < 0)
+				posicaoVerticalInicial -= velocidadeQueda;
 
-		if(posicaoVerticalInicial > 0 || velocidadeQueda < 0)
-			//posicaoVerticalInicial = posicaoVerticalInicial - velocidadeQueda
-			posicaoVerticalInicial -= velocidadeQueda ;
+			//Verifica se o cano saiu inteiramente da tela
+			if (posicaoMovimentoCanoHorizontal < -canoTopoMaior.getWidth()) {
+				posicaoMovimentoCanoHorizontal = larguraDispositivo;
+				alturaEntreCanosRandomica = numeroRandomico.nextInt(500) - 250;
+			}
 
-		//Verifica se o cano saiu inteiramente da tela
-		if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()){
-			posicaoMovimentoCanoHorizontal = larguraDispositivo;
-			alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200  ;
 		}
 
 		//Cria o batch , aqui dentro vai somente os objetos apresentados na tela
 		batch.begin(); 	//Inicio do batch
 
 		batch.draw(fundo,0,0, larguraDispositivo, alturaDispositivo );
+
 		batch.draw(canoTopoMaior,posicaoMovimentoCanoHorizontal , alturaDispositivo /2 + espacoEntreCanos /2 + alturaEntreCanosRandomica );
 		batch.draw(canoBaixoMaior,posicaoMovimentoCanoHorizontal , alturaDispositivo /2 - canoBaixoMaior.getHeight() - espacoEntreCanos /2 + alturaEntreCanosRandomica);
 		batch.draw(passaros[(int) variacaoPassaro], 120, posicaoVerticalInicial );
+		fonte.draw(batch , String.valueOf(pontuacao),larguraDispositivo / 2 , alturaDispositivo -50 );
 
 		batch.end();	//Final do batch
 
